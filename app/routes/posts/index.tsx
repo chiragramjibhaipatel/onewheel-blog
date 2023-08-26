050f528dceb1ba4467cc5706b574a8ee9ab4c989
Chirag Ramjibhai Patel <chiragramjibhaipatel@gmail.com>
2023-08-26 14:42:14 +0530

    move date to model

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,24 +1,12 @@
 import { Link, useLoaderData } from "@remix-run/react"
+import {json} from "@remix-run/node"
+import { getPosts } from "~/models/post.server"
 
 
 export const loader = async () => {
-    const posts = [
-      {
-        slug: "the-remix-conf",
-        title: "The remix conf 2023",
-      },
-      {
-        slug: "the-new-era-of-apps",
-        title: "The new era of the shopify apps",
-      },
-    ];
+    const posts = await getPosts();
 
-    const postsString = JSON.stringify({posts})
-    return new Response(postsString, {
-        headers: {
-            'Content-Type' : "application/json"
-        }
-    })
+   return json({posts})
 }
 
 export default function PostRoute(  ){
